08c391ea7513c6ee717e3d678f6d4064c6b6e0ed
a2623212 <a2623212@gmail.com>
2023-06-02 11:44:52 +0900

    feat: to follow the React guideline (like camelCase)

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -2,25 +2,29 @@ import React from "react";
 
 function GuessInput() {
   const [guess, setGuess] = React.useState("");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // recheck the validation
+    if (guess.length !== 5) {
+      window.alert("please input the 5-letter word 😄");
+      return;
+    }
 
+    console.log(
+      "🚀 ~ file: GuessInput.js:5 ~ GuessInput ~ uess:",
+      guess
+    );
+    setGuess("");
+  };
   return (
-    <form
-      class="guess-input-wrapper"
-      onSubmit={(event) => {
-        event.preventDefault();
-        console.log(
-          "🚀 ~ file: GuessInput.js:5 ~ GuessInput ~ uess:",
-          guess
-        );
-        setGuess("");
-      }}
-    >
-      <label for="guess-input">Enter guess:</label>
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+      <label htmlFor="guess-input">Enter guess:</label>
       <input
+        required
         id="guess-input"
         type="text"
-        minlength="5"
-        maxlength="5"
+        minLength="5"
+        maxLength="5"
         value={guess}
         onChange={(event) => {
           const nextGuess = event.target.value.toUpperCase();
