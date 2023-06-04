58996e3facd17a1f60742a63c0989e697f9a0104
a2623212 <a2623212@gmail.com>
2023-06-04 17:32:58 +0900

    feat: add a new component to show each single row of the guess

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,12 +1,18 @@
 import React from "react";
+import { range } from "../../utils";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
+import Guess from "../Guess";
 
 function GuessResults({ guesses }) {
+  console.log(
+    "🚀 ~ file: GuessResults.js:7 ~ GuessResults ~ guesses:",
+    guesses
+  );
+
   return (
     <div className="guess-results">
-      {guesses.map((guess, index) => (
-        <p className="guess" key={index}>
-          {guess}
-        </p>
+      {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
+        <Guess key={num} value={guesses[num]} />
       ))}
     </div>
   );
